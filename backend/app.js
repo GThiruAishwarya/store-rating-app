d44c5c4d3cdc93cdada4bcc7280a7d9249537ed7
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-12 07:08:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,17 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const storeRoutes = require('./routes/storeRoutes');
+const ratingRoutes = require('./routes/ratingRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
-// TODO: userRoutes, storeRoutes, ratingRoutes
+app.use('/api/users', userRoutes);
+app.use('/api/stores', storeRoutes);
+app.use('/api/ratings', ratingRoutes);
 
 module.exports = app;
